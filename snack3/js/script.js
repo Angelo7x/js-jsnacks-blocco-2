bd624252601882fdc53ca58a38972a847e871efd
Angelo7x <abennici96@gmail.com>
2021-08-27 13:07:54 +0200

    fix

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,13 +1,13 @@
 var nomi = ["Mario", "Giuseppe", "Angelo"];
 var cognomi = ["Rossi", "Verdi", "Bianchi"];
 
-for ( var i = 0; i < nomi.length; i++) {
+for ( var i = 0; i < 3; i++) {
 
     var randomNomi = Math.floor(Math.random() * nomi.length);
 
     var randomCognomi = Math.floor(Math.random() * cognomi.length);
 
     
-    console.log(nomi[randomNomi ] + cognomi[randomCognomi]);
+    console.log(nomi[ randomNomi ] + " " + cognomi[ randomCognomi ]);
 
 }
