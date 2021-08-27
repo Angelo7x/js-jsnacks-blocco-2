f21e8babaec20ed3803f31dcecc9adce7bf4d654
Angelo7x <abennici96@gmail.com>
2021-08-27 12:34:05 +0200

    validazione

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,5 +1,9 @@
 var numeroUtente = parseInt(prompt("Inserisci un numero"));
 
+while ( isNaN( numeroUtente ) ) {
+    numeroUtente = parseInt(prompt("Attento! Inserisci un numero"))
+}
+
 if (numeroUtente %2 == 0) {
     console.log(numeroUtente)
 } else {
