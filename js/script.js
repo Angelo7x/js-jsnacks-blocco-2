3964b1cf69d375710e8aff745cc5b6cf60dcbb5e
Angelo7x <abennici96@gmail.com>
2021-08-27 11:58:41 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var numeri = [];
+
+var sum = 0;
+
+for ( var i = 0; i < 5; i++) {
+    var numeroUtente = parseInt(prompt(" Inserisci un numero "));
+
+    numeri.push(numeroUtente);
+
+    var somma += numeri[i];
+
+    console.log(somma)
+
+}
+
+// var sommaNumeri = numeroUtente
+
+// document.getElementById("somma").innerHTML = "somma" += numeroUtente ;
